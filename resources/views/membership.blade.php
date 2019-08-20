47592639ed2a9e177fcc6d076add85f726928465
phyzerbert <xian1017@outlook.com>
2019-08-20 16:50:02 +0800

    cancelation

--- resources/views/membership.blade.php
+++ resources/views/membership.blade.php
@@ -285,12 +285,6 @@
     <script>
         $(document).ready(function(){
 
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
             $(".datepicker").datepicker({
                 orientation: 'auto bottom',
                 format: 'dd/mm/yyyy',
